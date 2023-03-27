e8b7311a9cb5da306fc4635f0968249d7a40c3a3
Shuo Wang <aphria@gmail.com>
2023-03-27 03:03:49 -0400

    Update train script for ucf101

--- model/ucf101_net_cnn_emb/train_1s.py
+++ model/ucf101_net_cnn_emb/train_1s.py
@@ -249,7 +249,7 @@ if __name__ == '__main__':
     opt.data_dir = osp.join(root(), "data", "nvbench", "dataset", "dataset_final")
     opt.db_info = osp.join(root(), "data", "nvbench", "dataset", "database_information.csv")
     opt.output_dir = "C:/Users/aphri/Documents/t0002/pycharm/data/ucf101/model_1s_2"
-    opt.epoch = 16
+    opt.epoch = 20
     opt.learning_rate = 0.005
     opt.batch_size = 10
 
